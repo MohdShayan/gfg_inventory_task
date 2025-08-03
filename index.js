7888b426b43c5af078b31a6c64b57c4e3fcf5863
MohdShayan <sehbaqureshi@gmail.com>
2025-08-03 15:27:00 +0530

    refactor: clean up code formatting and improve CORS settings

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express, { urlencoded } from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 import userRoutes from './routes/user.js';
-import productRoutes from './routes/product.js'
+import productRoutes from './routes/product.js';
 import { connectDb } from './ConnectDb.js';
 import cors from 'cors';
 
@@ -10,28 +10,26 @@ dotenv.config();
 
 const app = express();
 
+// Middleware
 app.use(express.json());
 app.use(cookieParser());
 app.use(urlencoded({ extended: false }));
 
-app.use(cors(
-    {
-        origin: ["http://localhost:5173","https://my-product.vercel.app"],
-        credentials: true,
-    }
-));
+// CORS settings
+app.use(cors({
+  origin: ["http://localhost:5173", "https://my-product.vercel.app"],
+  credentials: true,
+}));
 
-const PORT = process.env.PORT || 5000;
 
 app.use('/api/user', userRoutes);
-app.use('/api/product',productRoutes)
+app.use('/api/product', productRoutes);
+
 
 app.get('/', (req, res) => {
-    res.send('Hello World!GFG TASK2 SERVER ');
+  res.send('Hello World! GFG TASK2 SERVER');
 });
 
+connectDb();
 
-app.listen(PORT, () => {
-    connectDb();
-    console.log(`Server is running on port ${PORT}`);
-});
+export default app;
